d63a88ab8be57311511474a43ddede2fab0a68e7
hdrielb <hdrielb@gmail.com>
2023-02-07 23:18:00 +0200

    add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,44 @@
+import { themes } from "@storybook/theming";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  darkMode: {
+    current: "dark",
+    // Override the default dark theme
+    dark: { ...themes.dark, appBg: "black" },
+    // Override the default light theme
+    light: { ...themes.normal, appBg: "#eaeaea" },
+  },
+};
+
+export const decorators = [
+  (Story, { globals: { language } }) => {
+    console.log("context", language);
+    return <Story />;
+  },
+];
+
+export const globalTypes = {
+  language: {
+    name: "Theme",
+    description: "Styled-components theme",
+    defaultValue: "light",
+    toolbar: {
+      icon: "globe",
+      items: [
+        { value: "light", title: "Light theme" },
+        { value: "dark", title: "Dark theme" },
+      ],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+      // Change title based on selected value
+      dynamicTitle: true,
+    },
+  },
+};
