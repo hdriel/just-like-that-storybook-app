8147e1b3b913834a2063d99c3dc38d96179bd196
hdrielb <hdrielb@gmail.com>
2023-02-07 22:59:05 +0200

    clear project

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,5 @@
-import React, { useState } from "react";
-import { ThemeProvider } from "styled-components";
-import {
-  Title,
-  Button,
-  Spinner,
-  Input,
-  PasswordInputStyled,
-  Root,
-} from "./App.styled";
-import { ToggleTheme } from "./components/toggle-theme/ToggleTheme";
+import React from "react";
 
-// light theme
-const LightTheme = {
-  id: "light",
-  primaryColor: "#F8049C",
-  secondaryColor: "#fdd54f",
-  bodyBackgroundColor: "white",
-  bodyFontColor: "black",
-};
-
-// dark theme
-const DarkTheme = {
-  id: "dark",
-  primaryColor: "#f6f600",
-  secondaryColor: "midnightblue",
-  bodyBackgroundColor: "black",
-  bodyFontColor: "white",
-};
-
-const App = () => {
-  const [theme, setTheme] = useState(LightTheme);
-
-  return (
-    <ThemeProvider
-      theme={{
-        ...theme,
-        setTheme: () => {
-          setTheme((t) => (t.id === "light" ? DarkTheme : LightTheme));
-        },
-      }}
-    >
-      <Root>
-        <ToggleTheme />
-        <Title size={15} className="colored-text">
-          Hello React
-        </Title>
-      </Root>
-    </ThemeProvider>
-  );
-};
-
-export default App;
+export default function App() {
+  return <div>Just Like That - with Hadriel Benjo</div>;
+}
